ca1c6193aac212ce20bfa14a6780fc31f9fc9c15
std8 <saja.swalgah@yahoo.com>
2020-12-31 12:19:49 +0200

    my second flutter app

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+            centerTitle: true,
+          ),
+          body: MagicBall(),
+        ),
+      ),
+    );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  var num = 1;
+  void changBall() {
+    num = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  changBall();
+                });
+              },
+              child: Image.asset('images/ball$num.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
